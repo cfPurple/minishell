10e291170248fbe7f691de3739a6499f498199b5
cfPurple <97197921+cfPurple@users.noreply.github.com>
2024-01-14 12:36:44 +0100

    Update minishell.c

--- srcs/utils/minishell.c
+++ srcs/utils/minishell.c
@@ -17,15 +17,9 @@ void	minishell(int ac, char **av, char **env)
 	init_env(ac, av, env);
 	init_signal(0);
 	if (display_and_read() != SUCCESS)
-	{
 		ft_putstr_fd(ERROR_STOP, 2);
-		free_all_env(*get_env(0, 0, 0));
-		rl_clear_history();
-		printf("Minishell stopped with an error\n");
-		exit(1);
-	}
 	free_all_env(*get_env(0, 0, 0));
 	rl_clear_history();
 	printf("Minishell stopped normally\n");
-	exit(0);
+	exit(1);
 }
