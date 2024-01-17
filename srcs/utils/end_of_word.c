1064487489f8e7e0ef7a1a0678bc07988fa1ee0c
cfPurple <97197921+cfPurple@users.noreply.github.com>
2024-01-17 19:11:43 +0100

    Update end_of_word.c

--- srcs/utils/end_of_word.c
+++ srcs/utils/end_of_word.c
@@ -16,10 +16,10 @@ int	end_of_word(char *str, int i)
 {
 	if (!str[i])
 		return (i);
-	if (ft_strchr("<|>&", str[i]) != 0)
+	if (ft_strchr("<|>", str[i]) != 0)
 	{
 		i++;
-		if ((str[i - 1] != '|' && str[i - 1] != '&')
+		if ((str[i - 1] != '|')
 			&& (str[i] == '<' || str[i] == '>'))
 			i ++;
 		return (i);
@@ -27,7 +27,7 @@ int	end_of_word(char *str, int i)
 	while (str[i])
 	{
 		if (open_quote(str, i) == 0
-			&& (str[i] == ' ' || ft_strchr("<|>&", str[i]) != 0))
+			&& (str[i] == ' ' || ft_strchr("<|>", str[i]) != 0))
 			break ;
 		i++;
 	}
